3b7deb8a0e62246d1857e3cacbf53896c4321782
AlexRixten <rixten.09@gmail.com>
2024-04-03 00:56:09 +0300

    add useSessionStorage

--- src/shared/containers/app-header/app-header.tsx
+++ src/shared/containers/app-header/app-header.tsx
@@ -1,13 +1,16 @@
 import { Icon } from '../../ui/icon/icon.tsx';
 import styles from './app-header.module.css';
+import { useSessionStorage } from '../../hooks/use-session-storage.ts';
 export const AppHeader = () => {
+  const [userScorePoints] = useSessionStorage('user-score', 0);
+
   return (
     <header>
       <Icon name='sprite/logo' />
 
       <div className={styles.scoreWrap}>
         <span>Счет</span>
-        <p>32</p>
+        <p>{userScorePoints}</p>
       </div>
     </header>
   );
